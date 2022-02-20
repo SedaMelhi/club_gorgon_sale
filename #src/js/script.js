11a963c991905f14d8d54d2bd4f14b9fd61d3b31
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2022-02-20 03:47:53 +0300

    .
    .

--- #src/js/script.js
+++ #src/js/script.js
@@ -18,6 +18,95 @@ minus.addEventListener("click", function(){
     }
 })
 
+const left = document.querySelector(".left")
+const right = document.querySelector(".right")
+let leftCount = 1
+let rightCount = 1
+let index = 2
 
+left.addEventListener('click', function(){
+    if(leftCount < 2){
+        let bg = document.querySelector(`.bg-${index}`)
+        bg.classList.remove(`bg-${index}`)
+        index--
+        bg.classList.add(`bg-${index}`)
+        let leftImg = document.querySelector(".slick__item-left")
+        if(rightCount == 2){
+            document.querySelector('.hide_left').classList.add("slick__item-left")
+            document.querySelector('.hide_left').classList.remove('hide_left')
+            
+        }
+        let active = document.querySelector(".slick__item-active")
+       
+        let rightImg = document.querySelector(".slick__item-right")
+        leftImg.classList.add("slick__item-active")
+        leftImg.classList.remove("slick__item-left")
+        active.classList.remove("slick__item-active")
+        if(leftCount == 1){
+         rightImg.classList.add("hide_right")
+        }
+        active.classList.add("slick__item-right")
+        
+        leftCount++
+        rightCount--
+    }
+})
+right.addEventListener('click', function(){
+    if(rightCount < 2){
+        let bg = document.querySelector(`.bg-${index}`)
+        bg.classList.remove(`bg-${index}`)
+        index++
+        bg.classList.add(`bg-${index}`)
+        if(leftCount == 2){
+            document.querySelector('.hide_right').classList.remove('hide_right')
+        }
+        let active = document.querySelector(".slick__item-active")
+        let leftImg = document.querySelector(".slick__item-left")
+        let rightImg = document.querySelector(".slick__item-right")
+        rightImg.classList.add("slick__item-active")
+        rightImg.classList.remove("slick__item-right")
+        
+        active.classList.remove("slick__item-active")
+        if(rightCount == 1){
+            leftImg.classList.add("hide_left")
+            leftImg.classList.remove("slick__item-left")
+        }
+        active.classList.add("slick__item-left")
+        leftCount--
+        rightCount++
+    }
+   
+})
+/** 
+left.addEventListener('click', function(){
+    if (leftCount < 2){
+        let active = document.querySelector(".slick__item-active")
+        let leftImg = document.querySelector(".slick__item-left")
+        let rightImg = document.querySelector(".slick__item-right")
+        leftImg.classList.add("slide-pink-left")
+        active.classList.add("slide-blue-left")
+        rightImg.classList.add("slide-red-left")
+        leftCount++
+        rightCount--
+    }
+    
+})
 
-
+right.addEventListener('click', function(){
+    if(rightCount < 2){
+        let active = document.querySelector(".slick__item-active")
+        let leftImg = document.querySelector(".slick__item-left")
+        let rightImg = document.querySelector(".slick__item-right")
+        leftImg.classList.add("slide-pink-right")
+        active.classList.add("slide-blue-right")
+        rightImg.classList.add("slide-red-right")
+        active.classList.remove("slick__item-active")
+        leftImg.classList.remove("slick__item-left")
+        active.classList.add("slick__item-left")
+        rightImg.classList.add("slick__item-active")
+        leftCount--
+        rightCount++
+    }
+    
+})
+*/
--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,97 @@ minus.addEventListener("click", function () {
     count.value = value - 1;
   }
 });
+var left = document.querySelector(".left");
+var right = document.querySelector(".right");
+var leftCount = 1;
+var rightCount = 1;
+var index = 2;
+left.addEventListener('click', function () {
+  if (leftCount < 2) {
+    var bg = document.querySelector(".bg-".concat(index));
+    bg.classList.remove("bg-".concat(index));
+    index--;
+    bg.classList.add("bg-".concat(index));
+    var leftImg = document.querySelector(".slick__item-left");
+
+    if (rightCount == 2) {
+      document.querySelector('.hide_left').classList.add("slick__item-left");
+      document.querySelector('.hide_left').classList.remove('hide_left');
+    }
+
+    var active = document.querySelector(".slick__item-active");
+    var rightImg = document.querySelector(".slick__item-right");
+    leftImg.classList.add("slick__item-active");
+    leftImg.classList.remove("slick__item-left");
+    active.classList.remove("slick__item-active");
+
+    if (leftCount == 1) {
+      rightImg.classList.add("hide_right");
+    }
+
+    active.classList.add("slick__item-right");
+    leftCount++;
+    rightCount--;
+  }
+});
+right.addEventListener('click', function () {
+  if (rightCount < 2) {
+    var bg = document.querySelector(".bg-".concat(index));
+    bg.classList.remove("bg-".concat(index));
+    index++;
+    bg.classList.add("bg-".concat(index));
+
+    if (leftCount == 2) {
+      document.querySelector('.hide_right').classList.remove('hide_right');
+    }
+
+    var active = document.querySelector(".slick__item-active");
+    var leftImg = document.querySelector(".slick__item-left");
+    var rightImg = document.querySelector(".slick__item-right");
+    rightImg.classList.add("slick__item-active");
+    rightImg.classList.remove("slick__item-right");
+    active.classList.remove("slick__item-active");
+
+    if (rightCount == 1) {
+      leftImg.classList.add("hide_left");
+      leftImg.classList.remove("slick__item-left");
+    }
+
+    active.classList.add("slick__item-left");
+    leftCount--;
+    rightCount++;
+  }
+});
+/** 
+left.addEventListener('click', function(){
+    if (leftCount < 2){
+        let active = document.querySelector(".slick__item-active")
+        let leftImg = document.querySelector(".slick__item-left")
+        let rightImg = document.querySelector(".slick__item-right")
+        leftImg.classList.add("slide-pink-left")
+        active.classList.add("slide-blue-left")
+        rightImg.classList.add("slide-red-left")
+        leftCount++
+        rightCount--
+    }
+    
+})
+
+right.addEventListener('click', function(){
+    if(rightCount < 2){
+        let active = document.querySelector(".slick__item-active")
+        let leftImg = document.querySelector(".slick__item-left")
+        let rightImg = document.querySelector(".slick__item-right")
+        leftImg.classList.add("slide-pink-right")
+        active.classList.add("slide-blue-right")
+        rightImg.classList.add("slide-red-right")
+        active.classList.remove("slick__item-active")
+        leftImg.classList.remove("slick__item-left")
+        active.classList.add("slick__item-left")
+        rightImg.classList.add("slick__item-active")
+        leftCount--
+        rightCount++
+    }
+    
+})
+*/
